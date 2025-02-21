2969f40dabf489fe1c63037105fb109039e8a136
Vincent Prins <vincent.prins@ivdnt.org>
2025-02-21 11:39:53 +0100

    rm unzip-clean-corpus.sh
    no longer needed

--- unzip-and-clean-corpus.sh
+++
@@ -1,7 +0,0 @@
-# unzip all
-gunzip galahad-corpus-data/training-data/*/*.tsv.gz
-# Could attempt to use shopt -s globstar
-gunzip galahad-corpus-data/training-data/*/*/*.tsv.gz
-# Remove comments
-sed -i '/^#.*$/d' galahad-corpus-data/training-data/*/*.tsv
-sed -i '/^#.*$/d' galahad-corpus-data/training-data/*/*/*.tsv
