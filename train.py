d6dba6bd464a0d01b7d38e449c425924a8bd479c
Vincent Prins <146705420+PrinsINT@users.noreply.github.com>
2025-04-07 16:40:30 +0200

    Release 1.1.0 (#5)
    * Remove taggers-dockerized submodule
    
    In the end, a .gitkeep is more convenient.
    
    * Switch to codemeta-harvest
    
    So we don't have to keep updating the version
    
    * Update codemeta & readme
    
    * Indent datasets.json in docker folder
    
    * Update codemeta
    
    * Remove submodules
    
    Ultimately decided that it is very confusing to work with
    
    * update pie dockerfile prefab
    
    * rename trainingPath to path
    
    as in the corpus data repo
    
    * update readme
    
    * Explain tail -f log_file
    
    * rm unzip-clean-corpus.sh
    
    no longer needed
    
    * flush print before running tagger
    
    * Pie configs for DBNLDQ, ALL, 1200-1600, 1600-1900
    
    * Removed deprecated pie configs
    
    Consolidated into all, and century spans
    
    * ruff formatting
    
    * Updated pie docker prefab

--- train.py
+++ train.py
@@ -127,17 +127,15 @@ def add_dataset_provenance(docker_path: str) -> None:
     with open(f"{corpora_folder}/datasets.json") as provenance_file:
         provenance: list = json.load(provenance_file)
         for dataset in datasets:
-            # find object in list that matches trainingpath
-            matching = [
-                i for i in provenance if i["trainingPath"].split("/")[-1] == dataset
-            ]
+            # find object in list that matches path
+            matching = [i for i in provenance if i["path"].split("/")[-1] == dataset]
             if matching:
                 result["datasets"].append(matching[0])
             else:
                 result["datasets"].append({"name": dataset, "version": "unknown"})
 
     with open(f"{docker_path}/datasets.json", "w") as docker_file:
-        json.dump(result, docker_file)
+        json.dump(result, docker_file, indent=4)
 
 
 def run_tagger(
@@ -167,13 +165,16 @@ def run_tagger(
     if not os.path.exists(LOGS):
         os.makedirs(LOGS)
     # Run tagger
-    print(
-        f"Training tagger [{tagger_name}] with config [{config_name}]... (this will take a while)"
-    )
     # Note the -u to ensure flushing
     start_train_py = f"python3 -u {TAGGERS}/{tagger_name}/train.py {train_set_path} {dev_set_path} {config_path} {docker_path}"
     start_time = int(time.time())
-    log_path = f"{LOGS}/{config_name}-{start_time}.txt"
+    log_path = f"{LOGS}/{tagger_name}-{config_name}-{start_time}.txt"
+    print(
+        f"Training tagger [{tagger_name}] with config [{config_name}]... (this will take a while)"
+    )
+    print(f"To follow the progress, run: tail -f {log_path}")
+    # flush print before running
+    sys.stdout.flush()
     os.system(f"{activate_venv} && {start_train_py} > {log_path} 2>&1")
 
 
